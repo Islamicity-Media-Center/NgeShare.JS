c403fb54cc4c6d2cab559204c48e5124f3e302d4
Benn BIGGroup Al Islamicity <project.islamicity@gmail.com>
2024-03-10 05:50:46 +0700

    Update ngeshare.js
    Pertama, mari kita jelaskan bagian-bagian dari kode tersebut:
    
    1. `(function ($) { ... }(jQuery));`:
       - Ini adalah pola yang digunakan untuk membuat fungsi ekspresi yang akan dieksekusi secara langsung (Immediately Invoked Function Expression/IIFE). Dalam kasus ini, itu berarti kode di dalamnya akan dijalankan segera setelah didefinisikan. 
       - `$` adalah alias untuk objek jQuery.
       - `(jQuery)` adalah argumen yang diteruskan ke dalam fungsi tersebut, sehingga kita bisa menggunakan jQuery dalam lingkup fungsi tersebut.
    
    2. `var deskripsi = $('title').text();`:
       - Ini mengambil teks dari elemen `<title>` pada halaman web dan menyimpannya dalam variabel `deskripsi`. Ini digunakan untuk mendapatkan judul halaman web.
    
    3. `var core = { ... };`:
       - Ini adalah objek JavaScript yang berisi URL untuk berbagi ke platform media sosial seperti Twitter, Facebook, LinkedIn, Pinterest, dan email. Ini membantu dalam menghasilkan tautan berbagi yang sesuai.
    
    4. `$.fn.ngeshare = function () { ... };`:
       - Ini adalah cara untuk menambahkan metode baru ke objek jQuery, yang dalam kasus ini dinamakan `ngeshare`. Metode ini digunakan untuk membuat tombol berbagi dan menambahkannya ke elemen yang dipilih dengan jQuery.
    
    5. `var shareButtons = '<div class="ngeshare-shares">'; ...`:
       - Variabel `shareButtons` digunakan untuk membangun string HTML yang berisi tombol berbagi untuk platform media sosial yang disediakan oleh objek `core`. Setiap tombol ditambahkan ke string menggunakan operator `+=`.
    
    6. `this.append(shareButtons);`:
       - Ini menambahkan string `shareButtons` (yang berisi tombol berbagi) ke elemen yang dipilih dengan jQuery. Ini akan menambahkan tombol berbagi ke elemen yang memiliki kelas `ngeshare-shares`.
    
    7. `return this;`:
       - Ini mengembalikan elemen yang dipilih dengan jQuery untuk memungkinkan chaining (rantai) dalam penggunaan metode `ngeshare`.
    
    Demikianlah penjelasan satu per satu tentang arti dan fungsinya dari kode yang diberikan.

--- ngeshare.js
+++ ngeshare.js
@@ -1,19 +1,23 @@
-(function ( $ ) {
-var deskripsi = $('title').text();
-var core = {
-    twitter     : "https://twitter.com/share?url="+window.location.href+"&text="+deskripsi,
-    fb          : "https://www.facebook.com/sharer/sharer.php?u="+window.location.href,
-    linkedin    : "https://www.linkedin.com/shareArticle?mini=true&url="+window.location.href,
-    pinterest   : "https://pinterest.com/pin/create/bookmarklet/?media=http%3A%2F%2Fgdurl.com%2Fa653&url="+window.location.href+"&description="+deskripsi,
-    email       : "mailto:?subject="+deskripsi+"&body="+window.location.href
-};
-
-
-  $.fn.ngeshare = function(){
-        this.append('<div class="ngeshare-shares"><a href="'+core.fb+'" target="_blank" class="ngeshare-btn ngeshare-fb">Facebook</a><a target="_blank" href="'+core.twitter+'" class="ngeshare-btn ngeshare-twitter">Twiiter</a><a target="_blank" href="'+core.pinterest+'" class="ngeshare-btn ngeshare-pin">Pinterest</a><a target="_blank" href="'+core.linkedin+'" class="ngeshare-btn ngeshare-linkedin">Linkedin</a><a target="_blank" href="'+core.email+'" class="ngeshare-btn ngeshare-email">Email</a></div>');
-
-      return this;
+(function ($) {
+  var deskripsi = $('title').text();
+  var core = {
+    twitter: "https://twitter.com/share?url=" + window.location.href + "&text=" + deskripsi,
+    fb: "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href,
+    linkedin: "https://www.linkedin.com/shareArticle?mini=true&url=" + window.location.href,
+    pinterest: "https://pinterest.com/pin/create/bookmarklet/?media=http%3A%2F%2Fgdurl.com%2Fa653&url=" + window.location.href + "&description=" + deskripsi,
+    email: "mailto:?subject=" + deskripsi + "&body=" + window.location.href
   };
 
+  $.fn.ngeshare = function () {
+    var shareButtons = '<div class="ngeshare-shares">';
+    shareButtons += '<a href="' + core.fb + '" target="_blank" class="ngeshare-btn ngeshare-fb">Facebook</a>';
+    shareButtons += '<a target="_blank" href="' + core.twitter + '" class="ngeshare-btn ngeshare-twitter">Twitter</a>';
+    shareButtons += '<a target="_blank" href="' + core.pinterest + '" class="ngeshare-btn ngeshare-pin">Pinterest</a>';
+    shareButtons += '<a target="_blank" href="' + core.linkedin + '" class="ngeshare-btn ngeshare-linkedin">LinkedIn</a>';
+    shareButtons += '<a target="_blank" href="' + core.email + '" class="ngeshare-btn ngeshare-email">Email</a>';
+    shareButtons += '</div>';
 
-}( jQuery ));
+    this.append(shareButtons);
+    return this;
+  };
+}(jQuery));
